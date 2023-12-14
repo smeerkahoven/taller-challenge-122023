e5404eedb131b094aec9c86c3e04523e934ac1eb
Jose Lanza <ina_jlanza@contractor.indeed.com>
2023-12-14 04:42:46 -0400

    uploading files

---
+++ src/main/java/com/taller/challenge/Solution.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.taller.challenge;
+
+/**
+ *
+ * @author xeio
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public static int countSubstring(String value, int k) {
+        int size = value.length();
+        int count = 0;
+
+        for (int i = 0; i < size - k; i++) {
+            String substring = value.substring(i, i + k);
+            Set<Character> distinct = new HashSet<>();
+            for (char ch : substring.toCharArray()) {
+                distinct.add(ch);
+            }
+
+            if (distinct.size() == k) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ejecutando...");
+        if (args.length == 2) {
+            String value = args[0];
+            int k = Integer.parseInt(args[1]);
+            System.out.println("value :" + value);
+            System.out.println("k :" + k);
+            System.out.println(Solution.countSubstring(value, k));
+        }
+
+    }
+}
